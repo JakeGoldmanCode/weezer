2788507e4e204bc13d90a81003f949f67518ecb6
JakeGoldmanCode <95729626+JakeGoldmanCode@users.noreply.github.com>
2022-07-26 13:41:18 -0400

    things

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+function hideDumbthing() {
+    document.getElementById("backButton").style.display = "none";
+}
+
+function showDumbthing() {
+    document.getElementById("backButton").style.display = "block";
+}
+
+window.onload = hideDumbthing();
+
 function stupidButton() {
     alert("weezing time lol");
     window.open("https://www.youtube.com/watch?v=cQzMHhRCTYw");
+}
+function notSoStupidButtonHidden() {
+    alert("wow you found the special button!!!");
+    document.getElementById("text").style.display = "none";
+    document.getElementById("description").style.display = "none";
+    document.getElementById("images").style.display = "none";
+    document.getElementById("buttons").style.diplay = "none";
+    showDumbthing();
+}
+
+function dumbthingDoesSomething() {
+    document.getElementById("text").style.display = "block";
+    document.getElementById("description").style.display = "block";
+    document.getElementById("images").style.display = "block";
+    document.getElementById("buttons").style.diplay = "block";
+    hideDumbthing();
 }
