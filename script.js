5cd80a22a408b610fbd469821f7e0e2b7953294c
JakeGoldmanCode <95729626+JakeGoldmanCode@users.noreply.github.com>
2022-07-26 12:42:52 -0400

    added lots of stuff

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function stupidButton() {
     alert("weezing time lol");
     window.open("https://www.youtube.com/watch?v=cQzMHhRCTYw");
 }
+function notSoStupidButtonHidden() {
+    alert("wow you found the special button!!!");
+    document.getElementById("text").style.display = "none";
+    document.getElementById("images").style.display = "none";
+}
