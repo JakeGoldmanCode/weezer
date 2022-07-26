1b8f824eab36ce489eea7c962a7ffb9ce5e50147
JakeGoldmanCode <95729626+JakeGoldmanCode@users.noreply.github.com>
2022-07-26 12:56:12 -0400

    testing things

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function hiddenButton() {
+    document.getElementById("back").style.display = "none";
+}
+
+window.onload = hiddenButton;
+
 function stupidButton() {
     alert("weezing time lol");
     window.open("https://www.youtube.com/watch?v=cQzMHhRCTYw");
@@ -7,4 +13,6 @@ function notSoStupidButtonHidden() {
     document.getElementById("text").style.display = "none";
     document.getElementById("description").style.display = "none";
     document.getElementById("images").style.display = "none";
+    document.getElementById("buttons").style.diplay = "none";
+    document.getElementById("back").style.display = "block";
 }
