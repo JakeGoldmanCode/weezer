45bb67bdc8405b48a1bbad1d100995b1170d668c
JakeGoldmanCode <95729626+JakeGoldmanCode@users.noreply.github.com>
2022-07-26 12:43:39 -0400

    more changes

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ function stupidButton() {
 function notSoStupidButtonHidden() {
     alert("wow you found the special button!!!");
     document.getElementById("text").style.display = "none";
+    document.getElementById("description").style.display = "none";
     document.getElementById("images").style.display = "none";
 }
